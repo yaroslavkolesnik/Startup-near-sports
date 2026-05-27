01536f00f2594ceaeaf4a61acd4c7a34ddaca343
Yaroslav Kolisnuk <02.07.2005k@gmail.com>
2026-05-27 14:05:13 +0300

    added rules for passwords

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,6 +65,24 @@ DATABASES = {
     }
 }
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
 from .models import UserModel
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,6 +15,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ['username', 'email', 'password', 'avatar', 'sport_skills', 'preferred_sports']
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(list(e.messages))
+        return value
 
     def create(self, validated_data):
         user = UserModel.objects.create_user(
